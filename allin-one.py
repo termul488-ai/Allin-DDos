ef1ef770b915f39cdeb46d62dc73f6112fb750b7
termul488-ai <termul488@gmail.com>
2026-01-09 23:58:38 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -220,8 +220,7 @@ def main():
         print("\033[32m┗━━━> UDP Random press 2")
         method = input("\033[7m Select method (1-2):\033[0m\033[32m \033[0m").strip()
 
-        print("\033[32m┏━━━━━━━⬣")
-        ip = input("\033[32m┗> IP Server: ")
+        ip = input(" IP Server: ")
         port = validate_input("\033[32m┗> port (80/443):  , 1, 65535")
         duration = validate_input("\033[32m┗> flood duration in seconds: " , 1, float('inf'), float")
         packet_size = validate_input("\033[32m┗> packet size in bytes (1-65500): " , 1, 65500")
