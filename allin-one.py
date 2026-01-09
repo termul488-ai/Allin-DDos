6a5dd8c7ff0ab63f821710b85534a9981d602716
termul488-ai <termul488@gmail.com>
2026-01-09 20:38:15 +0700

    Update allin-one.py

--- allin-one.py
+++ allin-one.py
@@ -195,11 +195,12 @@ def main():
     print(f"{Fore.LIGHTRED_EX}╚{'═' * 55}╝")
 
     while attemps < 100:
+        print("\033[32m
         username = input("\033[32mEnter your username: \033[0m")
         password = input("\033[32mEnter your password: \033[0m")
 
     if username == 'ba313' and password == 'ba313':
-        print("\033[48;5;3m•••⟩⟩ R U A N G  P E J U A N G \033[0m")
+        print("\033[48;5;3m•••⟩⟩ R U A N G  P E J U A N G ...!!\033[0m")
         break
     else:
         print('Incorrect credentials. Check if you have Caps lock on and try again.')
